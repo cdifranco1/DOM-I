133bb9ccb4bb522860cfac3485ce4ec9ea6b2e93
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-02-10 14:06:52 -0600

    completed project besides stretch

--- js/index.js
+++ js/index.js
@@ -40,3 +40,107 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Set links text
+let links = Array.from(document.getElementsByTagName('a'))
+for (let i = 0; i < links.length; i++){
+  console.log(siteContent["nav"][`nav-item-${i + 1}`])
+  links[i].textContent = siteContent["nav"][`nav-item-${i + 1}`];
+}
+
+// Update CTA header, button, img
+document.querySelector('.cta-text h1').textContent = siteContent.cta.h1;
+
+document.querySelector('.cta-text button').textContent = siteContent.cta.button;
+
+document.querySelector('#cta-img').src = siteContent["cta"]["img-src"]
+
+
+//Main content
+
+//Features
+document.querySelector('.top-content .text-content:first-child h4').textContent = siteContent["main-content"]["features-h4"]
+
+document.querySelector('.top-content .text-content:first-child p').textContent = siteContent["main-content"]["features-content"]
+
+//About
+document.querySelector('.top-content .text-content:last-child h4').textContent = siteContent["main-content"]["about-h4"]
+
+document.querySelector('.top-content .text-content:last-child p').textContent = siteContent["main-content"]["about-content"]
+
+//Middle img
+document.querySelector('#middle-img').src = siteContent["main-content"]["middle-img-src"]
+
+
+//Bottom content
+
+document.querySelector('.bottom-content .text-content:first-child h4').textContent = siteContent["main-content"]["services-h4"]
+
+document.querySelector('.bottom-content .text-content:first-child p').textContent = siteContent["main-content"]["services-content"]
+
+document.querySelector('.bottom-content .text-content:nth-child(2) h4').textContent = siteContent["main-content"]["product-h4"]
+
+document.querySelector('.bottom-content .text-content:nth-child(2) p').textContent = siteContent["main-content"]["product-content"]
+
+document.querySelector('.bottom-content .text-content:last-child h4').textContent = siteContent["main-content"]["vision-h4"]
+
+document.querySelector('.bottom-content .text-content:last-child p').textContent = siteContent["main-content"]["vision-content"]
+
+
+//Contact
+
+document.querySelector('.contact h4').textContent = siteContent["contact"]["contact-h4"]
+
+
+document.querySelector('.contact p:nth-child(2)').textContent = siteContent.contact.address
+
+document.querySelector('.contact p:nth-child(3)').textContent = siteContent.contact.phone
+
+document.querySelector('.contact p:nth-child(4)').textContent = siteContent.contact.email
+
+//Footer
+
+document.querySelector('footer p').textContent = siteContent.footer.copyright
+
+
+
+//Task 3
+
+//Nav text to green
+
+links.forEach((item) => item.style.color = 'green')
+
+
+//Create new elements
+const linkOne = document.createElement('a')
+linkOne.textContent = 'First Link'
+linkOne.href = '#'
+document.querySelector('nav').prepend(linkOne)
+
+const linkTwo = document.createElement('a')
+linkTwo.href = '#'
+linkTwo.textContent = 'Second Link'
+
+document.querySelector('nav').appendChild(linkTwo)
+
+
+
+//Stretch Task
+
+const newButton = document.createElement("button");
+newButton.textContent = 'Change background';
+
+document.querySelector('.cta-text').appendChild(newButton).style.background = 'grey';
+
+newButton.style.borderRadius = '5px'
+newButton.style.border = '2px solid black'
+newButton.style.width = '60%'
+
+newButton.addEventListener("click", () => {
+  let background = document.querySelector('.cta') 
+  if (background.style.backgroundColor == ""){
+    background.style.backgroundColor = "lightgrey"
+  } else {background.style.backgroundColor = ""}
+})
+
+
